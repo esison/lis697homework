8d434128b200a8ea33a2b3a865710776130cc4a1
esison <esison@pratt.edu>
2015-09-07 23:06:04 -0400

    Second change
    Still testing

--- first.php
+++ first.php
@@ -2,7 +2,8 @@
 <body>
 <?php
 echo "Hello world!";
-echo "A change happened here"
+echo "A change happened here";
+echo "To view the files in the browser: http://localhost/lis697homework/first.php";
 ?>
 </body>
 </html>
